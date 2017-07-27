5571d77155ab6e940160ccc8b1c85d6dd816ec82
ShafiqEssani <shafiqshamsessani@gmail.com>
2017-07-27 13:54:25 +0500

    initial commit

---
+++ app/ts/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+import {Config} from './config.service';
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{mainHeading}}</h1>'
+})
+
+export class AppComponent {
+  mainHeading = Config.MYVAR;
+}
